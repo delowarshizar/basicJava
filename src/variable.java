2a3d3cd2accc4e01ce12fd6ffe4dd505bcc670c6
delowarshizar <husain22205101453@diu.edu.bd>
2025-07-31 01:02:27 +0600

    added data types

--- src/variable.java
+++ src/variable.java
@@ -1,5 +1,6 @@
 public class variable {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println(4+3);
     }
 }
---
+++ src/varible.java
@@ -0,0 +1,13 @@
+public class varible {
+    public static void main(String[] args) {
+        int num1 = 10;
+        float num2 = 23.2f;
+        double num3 = 4.32;
+        char alpha = 'S';
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num1*num2*num3);
+        System.out.println(alpha);
+    }
+}
